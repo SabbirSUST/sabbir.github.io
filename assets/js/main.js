a9a2a05eb62d65c3c4f96e7147e21bc2b67eaa23
Mohammed Sabbir <sabbir.cse.sust@gmail.com>
2023-09-14 11:47:38 +0600

    update site link

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /**
-* Template Name: Impact - v1.2.0
+* Template Name: Mohammed Sabbir - v1.2.0
 * Template URL: https://bootstrapmade.com/impact-bootstrap-business-website-template/
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
